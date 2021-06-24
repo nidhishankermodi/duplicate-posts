bda476ed7188bdfdb44b3218b79e220e9a1deab6
nsmodi <nidhishankermodi2012@gmail.com>
2021-06-24 11:01:34 +0530

    Added translation in plugin.

--- postduplicator.php
+++ postduplicator.php
@@ -58,19 +58,19 @@ function nsmodi_pd_duplicate_post_as_draft() {
 		 * new post data array
 		 */
 		$args = array(
-			'comment_status' => $post->comment_status,
-			'ping_status' => $post->ping_status,
-			'post_author' => $new_post_author,
-			'post_content' => $post->post_content,
-			'post_excerpt' => $post->post_excerpt,
-			'post_name' => $post->post_name,
-			'post_parent' => $post->post_parent,
-			'post_password' => $post->post_password,
-			'post_status' => 'draft',
-			'post_title' => $post->post_title,
-			'post_type' => $post->post_type,
-			'to_ping' => $post->to_ping,
-			'menu_order' => $post->menu_order
+				'comment_status' => $post->comment_status,
+				'ping_status' => $post->ping_status,
+				'post_author' => $new_post_author,
+				'post_content' => $post->post_content,
+				'post_excerpt' => $post->post_excerpt,
+				'post_name' => $post->post_name,
+				'post_parent' => $post->post_parent,
+				'post_password' => $post->post_password,
+				'post_status' => 'draft',
+				'post_title' => $post->post_title,
+				'post_type' => $post->post_type,
+				'to_ping' => $post->to_ping,
+				'menu_order' => $post->menu_order
 		);
 		/*
 		 * insert the post by wp_insert_post() function
@@ -80,7 +80,7 @@ function nsmodi_pd_duplicate_post_as_draft() {
 		 * get all current post terms ad set them to the new post draft
 		 */
 		$taxonomies = get_object_taxonomies(
-			$post->post_type ); // returns array of taxonomy names for post type, ex array("category", "post_tag");
+				$post->post_type ); // returns array of taxonomy names for post type, ex array("category", "post_tag");
 		foreach ( $taxonomies as $taxonomy ) {
 			$post_terms = wp_get_object_terms( $post_id, $taxonomy, array('fields' => 'slugs') );
 			wp_set_object_terms( $new_post_id, $post_terms, $taxonomy, false );
@@ -89,7 +89,7 @@ function nsmodi_pd_duplicate_post_as_draft() {
 		 * duplicate all post meta just in two SQL queries
 		 */
 		$post_meta_infos = $wpdb->get_results(
-			"SELECT meta_key, meta_value FROM $wpdb->postmeta WHERE post_id=$post_id" );
+				"SELECT meta_key, meta_value FROM $wpdb->postmeta WHERE post_id=$post_id" );
 		if ( count( $post_meta_infos ) != 0 ) {
 			$sql_query = "INSERT INTO $wpdb->postmeta (post_id, meta_key, meta_value) ";
 			foreach ( $post_meta_infos as $meta_info ) {
@@ -118,8 +118,8 @@ add_action( 'admin_action_nsmodi_pd_duplicate_post_as_draft', 'nsmodi_pd_duplica
 function nsmodi_pd_duplicate_post_link( $actions, $post ) {
 	if ( current_user_can( 'edit_posts' ) ) {
 		$actions[ 'duplicate' ] = '<a href="' . wp_nonce_url(
-				'admin.php?action=nsmodi_pd_duplicate_post_as_draft&post=' . $post->ID, basename( __FILE__ ),
-				'duplicate_nonce' ) . '" title="Duplicate this item" rel="permalink">Duplicate</a>';
+						'admin.php?action=nsmodi_pd_duplicate_post_as_draft&post=' . $post->ID, basename( __FILE__ ),
+						'duplicate_nonce' ) . '" title="Duplicate this item" rel="permalink">Duplicate</a>';
 	}
 	return $actions;
 }
--- readme.txt
+++ readme.txt
@@ -2,7 +2,7 @@
 Contributors: Nidhishanker Modi
 Tags: Post Duplicator, Pages, Posts, Duplicate wordpress posts, Clone a post, clone pages
 Requires at least: 5.6
-Tested up to:  5.7.4
+Tested up to:  5.7.2
 Stable tag: 1.0
 Requires PHP: 5.3.2
 Donate link:
@@ -14,9 +14,18 @@ License URI: http://www.gnu.org/licenses/gpl-3.0.html
 == Description ==
 "Wordpress Post Duplicator" provides a feature to duplicate wordpress post type such posts, pages & products.
 
-**Features provided with this plugin:**
+**How to duplicate:**
+
+1. Open the page or post you want to duplicate.
+2. In the post listing, there would be button duplicate after view link.
+3. Click on duplicate.
+4. The post will be duplicated and saved as a new post named as (copy) with post title and make it drafted.
+5. System will open Edit duplicated posts.
 
 == Installation ==
+
+1. Install the plugin like you always install plugins, either by uploading it via FTP or by using the "Add Plugin" function of WordPress.
+2. Activate the plugin at the plugin administration page
 
 = Automatic installation =
 
@@ -26,10 +35,14 @@ Install Wordpress Post Duplicator just like any other WordPress plugin.
 
 == Frequently Asked Questions ==
 
-= Will this plugin work with my theme? =
-Yes, it will work with any theme, but may require some styling to make it match nicely.
+= Will this plugin work with my all post types? =
+Right now, it is tested with WordPress Posts/Pages/Products and it is working fine.
 
 == Screenshots ==
+
+1. Duplicate Wordpress Blogs
+2. Duplicate Wordpress Pages
+3. Duplicate Wordpress Products
 
 
 == Upgrade Notice ==
